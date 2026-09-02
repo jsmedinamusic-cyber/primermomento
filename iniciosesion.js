ccb9eb271fce690f93fe37aed673ba3e80553283
jsmedinamusic-cyber <jsmedinamusic@gmail.com>
2026-09-02 18:02:45 -0500

    codigo terminado

--- iniciosesion.js
+++ iniciosesion.js
@@ -1,15 +1,25 @@
-<script>
-   const usuarioCorrecto = "admin";
-   const contrasenaCorrecta = "1234";
+const usuarioCorrecto = "admin";
+const contrasenaCorrecta = "1234";
 
-   let intentos = 0;
-   const maxIntentos = 3;
-   let sesionIniciada = false;
-   
-   while (intentos < maxIntentos && sesionIniciada === false) {
-      let usuario = prompt("Ingrese su usuario:");
-      let contrasena = prompt("Ingrese su contraseña:");
+let intentos = 0;
+const maxIntentos = 3;
+let sesionIniciada = false;
 
-   if (usuario === usuarioCorrecto && contrasena === contrasenaCorrecta)
-   {alert("Bienvenido " + usuario);
-    sesionIniciada = true;      } 
+while (intentos < maxIntentos && sesionIniciada === false) {
+   let usuario = prompt("Ingrese su usuario:");
+   let contrasena = prompt("Ingrese su contraseña:");
+
+   if (usuario === usuarioCorrecto && contrasena === contrasenaCorrecta) {
+      alert("Bienvenido " + usuario);
+      sesionIniciada = true;
+   } else {
+      intentos = intentos + 1;
+      let intentosRestantes = maxIntentos - intentos;
+
+      if (intentosRestantes > 0) {
+         alert("Usuario o contraseña incorrectos. Le quedan " + intentosRestantes + " intento(s).");
+      } else {
+         alert("Ha superado el número máximo de intentos. Saliendo...");
+      }
+   }
+}
